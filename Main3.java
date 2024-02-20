a67fed5204fca8848746ae5851cf890d1b38eb52
EmirB69 <emir.bajramovic2021@treca-gimnazija.edu.ba>
2024-02-20 10:59:33 +0100

    3

---
+++ Main3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main3 {
+    public static void main(String[] args) {
+    int n;
+
+    System.out.print ("Ispis brojeva od 1 do 10: ");
+
+    for(int i = 1; i<=10;i++){
+        System.out.print(i + " ");
+    }
+}
+}
