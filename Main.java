92c1a3eefa44a967cfea6cd2e8634525d9a1134f
EmirB69 <emir.bajramovic2021@treca-gimnazija.edu.ba>
2024-02-20 10:19:51 +0100

    Zadatak2

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    String rijec;
+    int brojSlova;
+    Scanner ulaz = new Scanner(System.in);
+
+    System.out.println("Unesite string:");
+    rijec = ulaz.nextLine();
+    brojSlova = rijec.length();
+
+    System.out.println("Broj karaktera u stringu: " + brojSlova);
+    }
+}
