07dcf495bd8d00990abf2cc5ce131ff1e3b5d2b5
EmirB69 <emir.bajramovic2021@treca-gimnazija.edu.ba>
2024-02-20 11:24:53 +0100

    4

--- Main.java
+++ Main.java
@@ -2,14 +2,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    String rijec;
-    int brojSlova;
+    int niz[] = new int[5];
+    int suma = 0;
     Scanner ulaz = new Scanner(System.in);
 
-    System.out.println("Unesite string:");
-    rijec = ulaz.nextLine();
-    brojSlova = rijec.length();
-
-    System.out.println("Broj karaktera u stringu: " + brojSlova);
+    for(int i = 0; i < 5; i++)
+    {
+        System.out.println("Unesi "+ (i + 1) + ". broj: ");
+        niz[i] = ulaz.nextInt();
+        suma += niz[i];
     }
+    System.out.println("Suma unesenih brojeva je: " + suma);
 }
+}
+
